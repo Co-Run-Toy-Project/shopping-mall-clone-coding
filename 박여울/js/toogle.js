91bd84019a6e8c560729c364b763717ef89b99e6
yeowool <qyoong3579@gmail.com>
2022-08-23 19:35:03 +0900

    1주차 업로드

---
+++ 박여울/js/toogle.js
@@ -0,0 +1,7 @@
+const togleBtn = document.querySelector('.toggle-side-bar');
+const menuBox = document.querySelector('.side-bar');
+
+
+togleBtn.addEventListener('click', () => {
+    menuBox.classList.toggle(active);
+})
